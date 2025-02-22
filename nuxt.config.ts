8789cdefed0aa9af7bc2496daf78fd0687ae797a
Melvin Borja <melvin.borja@petnet.com.ph>
2025-02-22 15:42:19 +0800

    feat: Add basic meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Convertimg - A simple app to convert image to another image format online for free',
+      meta: [
+        { name: 'og:title', content: 'Convertimg' },
+        { name: 'og:description', content: 'A simple app to convert image to another image format online for free' }
+      ]
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
